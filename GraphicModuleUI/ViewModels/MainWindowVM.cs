ddfececb9bb7389dbcff991936dea05bf539a0a8
Stepanyuga Anton <ganeball9799@mail.ru>
2021-05-07 00:18:56 +0700

    Initial commit

---
+++ GraphicModuleUI/ViewModels/MainWindowVM.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using GalaSoft.MvvmLight;
+
+namespace GraphicModuleUI.ViewModel
+{
+    public class MainWindowVM : ViewModelBase
+    {
+        public MainWindowVM()
+        {
+        }
+    }
+}
