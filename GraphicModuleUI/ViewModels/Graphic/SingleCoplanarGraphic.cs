920d1eb6d28b061d7329bcf73958dff3825c6be2
Stepanyuga Anton <ganeball9799@mail.ru>
2021-05-21 13:54:24 +0700

    Исправлена коплонарная линия(строиться относительно центра)

--- GraphicModuleUI/ViewModels/Graphic/SingleCoplanarGraphic.cs
+++ GraphicModuleUI/ViewModels/Graphic/SingleCoplanarGraphic.cs
@@ -17,7 +17,7 @@ namespace GraphicModuleUI.ViewModels.Graphic
         public SingleCoplanarGraphic( Geometry geometry)
         {
             _geometry = geometry;
-            Canvas.SetLeft(this, 100);
+            Canvas.SetLeft(this, 200);
             Canvas.SetTop(this, 200);
         }
 
@@ -96,11 +96,11 @@ namespace GraphicModuleUI.ViewModels.Graphic
 
         protected override void OnRender(DrawingContext dc)
         {
-            S2 = _geometry[ParameterName.Slot, 1].Value*2;
-            S1 = _geometry[ParameterName.Slot, 0].Value*2;
-            W1 = _geometry[ParameterName.StripWidth].Value * 2;
-            t = _geometry[ParameterName.StripsThickness].Value * 2;
-            h = _geometry[ParameterName.SubstrateHeight].Value*2;
+            S2 = _geometry[ParameterName.Slot, 1].Value;
+            S1 = _geometry[ParameterName.Slot, 0].Value;
+            W1 = _geometry[ParameterName.StripWidth].Value;
+            t = _geometry[ParameterName.StripsThickness].Value;
+            h = _geometry[ParameterName.SubstrateHeight].Value;
             var g = 20;
             base.OnRender(dc);
             var wSolidBrush = new SolidColorBrush(Colors.Blue);
@@ -117,12 +117,24 @@ namespace GraphicModuleUI.ViewModels.Graphic
             var textThickness = new FormattedText("t", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
                 new Typeface("verdana"), 8, Brushes.Red);
 
+
             var myPen = new Pen(Brushes.Black, 0.1);
             var penLine = new Pen(Brushes.Red, 1);
-            var substrateRect = new Rect(0, 0, W1+ S1 + S2+g*2, h);
-            var widthRect = new Rect(g+ S1, -t,W1,t);
-            var groundLeft = new Rect(0, -t,g,t);
-            var groundRight = new Rect(g+ S1 + S2+W1,-t,g,t);
+            var substrateRect = new Rect(-(g+S1+W1/2), 0, W1 + S1 + S2 + g * 2, h);
+            var widthRect = new Rect(-W1/2, -t, W1, t);
+            var groundLeft = new Rect(-(g+S1+W1/2), -t, g, t);
+            var groundRight = new Rect(W1/2+S2, -t, g, t);
+
+
+
+
+            //Рабочая часть
+            //var myPen = new Pen(Brushes.Black, 0.1);
+            //var penLine = new Pen(Brushes.Red, 1);
+            //var substrateRect = new Rect(0, 0, W1+ S1 + S2+g*2, h);
+            //var widthRect = new Rect(g+ S1, -t,W1,t);
+            //var groundLeft = new Rect(0, -t,g,t);
+            //var groundRight = new Rect(g+ S1 + S2+W1,-t,g,t);
 
             dc.DrawRectangle(wSolidBrush, myPen, widthRect);
             dc.DrawRectangle(hSolidBrsh, myPen, substrateRect);
@@ -130,24 +142,26 @@ namespace GraphicModuleUI.ViewModels.Graphic
             dc.DrawRectangle(groundBrush, myPen, groundRight);
 
             //Линии разделения для слотов и ширины
-            dc.DrawLine(penLine, new Point(g,-t),new Point(g,-(t+20)));
-            dc.DrawLine(penLine, new Point(g + S1, -t), new Point(g + S1, -(t + 20)));
-            dc.DrawLine(penLine, new Point(g + S1+W1, -t), new Point(g + S1+W1, -(t + 20)));
-            dc.DrawLine(penLine, new Point(g + S1 + W1+S2, -t), new Point(g + S1 + W1 + S2, -(t + 20)));
-            dc.DrawLine(penLine, new Point(g-5, -(t + 15)), new Point(g + S1 + W1 + S2, -(t + 15)));
-            dc.DrawText(textWidth, new Point(g + S1+W1/2-3, -(t + 25)));
-            dc.DrawText(textS1, new Point(g + S1/2-3, -(t + 25)));
-            dc.DrawText(textS2, new Point(g + S1 + S2/2 + W1-3, -(t + 25)));
+            dc.DrawLine(penLine, new Point(-(S1+W1/2),-t),new Point(-(S1 + W1 / 2), -(t+20)));
+            dc.DrawLine(penLine, new Point(-W1 / 2, -t), new Point(-W1 / 2, -(t + 20)));
+            dc.DrawLine(penLine, new Point(W1/2, -t), new Point(W1/2, -(t + 20)));
+            dc.DrawLine(penLine, new Point(W1/2+S2, -t), new Point(W1/2 + S2, -(t + 20)));
+            dc.DrawLine(penLine, new Point(-(S1 + W1 / 2+5), -(t + 15)), new Point(W1/2+S2+5, -(t + 15)));
+            dc.DrawText(textWidth, new Point(-3, -(t + 25)));
+            dc.DrawText(textS1, new Point(-(g+S1/2), -(t + 25)));
+            dc.DrawText(textS2, new Point(W1/2+S1/2, -(t + 25)));
 
             //Линии для разделения толщины подложки
-            dc.DrawLine(penLine, new Point(0, 0), new Point(-20, 0));
-            dc.DrawLine(penLine, new Point(0, h), new Point(-20, h));
-            dc.DrawText(textSubstrateHeight, new Point(-30, h/2-3));
+            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), 0), new Point(-(25+g+S1+W1/2), 0));
+            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), h), new Point(-(25 + g + S1 + W1 / 2), h));
+            dc.DrawText(textSubstrateHeight, new Point(-(30 + g + S1 + W1 / 2), h / 2 - 3));
 
             //Линии для разделения толщины линии
-            dc.DrawLine(penLine, new Point(0, -t), new Point(-25, -t));
-            dc.DrawLine(penLine, new Point(-20, -5-t), new Point(-20, h+5));
-            dc.DrawText(textThickness, new Point(-30, -t / 2 -5));
+            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), -t), new Point(-(25 + g + S1 + W1 / 2), -t));
+            dc.DrawLine(penLine, new Point(-(20 + g + S1 + W1 / 2), -5-t), new Point(-(20 + g + S1 + W1 / 2), h+5));
+            dc.DrawText(textThickness, new Point(-(30 + g + S1 + W1 / 2), -t / 2 -5));
+
+
 
         }
     }
