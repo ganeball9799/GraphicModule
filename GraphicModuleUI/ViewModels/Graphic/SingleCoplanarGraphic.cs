54c8606760f0b92778765a8ef92f1e395325bb75
Stepanyuga Anton <ganeball9799@mail.ru>
2021-05-29 00:48:29 +0700

    Корректировка копланарной линии

--- GraphicModuleUI/ViewModels/Graphic/SingleCoplanarGraphic.cs
+++ GraphicModuleUI/ViewModels/Graphic/SingleCoplanarGraphic.cs
@@ -14,91 +14,80 @@ namespace GraphicModuleUI.ViewModels.Graphic
     {
         private Geometry _geometry;
 
-        private double slot1;
-        private double slot2;
+        private List<double> _slots = new List<double>();
 
-        private double stripWidth;
-        private double stripsThicknees;
-        private double substrateHeight;
-
-        private const double g=10;
-        private const double textSize=5;
-
-        private double zoomt;
-        private double zoomh;
-        private double zoomw;
-        private double zooms1;
-        private double zooms2;
-
+        private double _stripWidth;
+        private double _stripsThicknees;
+        private double _substrateHeight;
+        
         public SingleCoplanarGraphic(Geometry geometry)
         {
             _geometry = geometry;
 
-            slot1 = _geometry[ParameterName.Slot].Value;
-            slot2 = _geometry[ParameterName.Slot,1].Value;
-            stripWidth = _geometry[ParameterName.StripWidth].Value;
-            stripsThicknees = _geometry[ParameterName.StripsThickness].Value;
-            substrateHeight = _geometry[ParameterName.SubstrateHeight].Value;
+            _slots.Add(_geometry[ParameterName.Slot].Value);
+            _slots.Add(_geometry[ParameterName.Slot, 1].Value);
+            _stripWidth = _geometry[ParameterName.StripWidth].Value;
+            _stripsThicknees = _geometry[ParameterName.StripsThickness].Value;
+            _substrateHeight = _geometry[ParameterName.SubstrateHeight].Value;
 
             Canvas.SetLeft(this, 120);
             Canvas.SetTop(this, 120);
         }
 
+        private double Zoom(double zoom)
+        {
+            if (zoom > 2.5)
+            {
+                zoom = 2.5;
+            }
+            else if (zoom < 0.3)
+            {
+                zoom = 0.3;
+            }
+            return zoom;
+        }
+
+        private FormattedText Text(string mesure,double textSize)
+        {
+            var text = new FormattedText(mesure, CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
+                new Typeface("verdana"), textSize, Brushes.DarkBlue);
+            return text;
+        }
+
         protected override void OnRender(DrawingContext dc)
         {
             base.OnRender(dc);
-            zoomt = (stripsThicknees / substrateHeight);
-            zoomh = (substrateHeight / stripsThicknees);
-            zooms1 = slot1 / ((slot2 + stripWidth) / 2);
-            zooms2 = slot2 / ((slot1 + stripWidth) / 2);
-            zoomw = stripWidth / ((slot1 + slot2) / 2);
+            var screen = 10;
+            var textSize = 6;
+            var zoomt = (_stripsThicknees / _substrateHeight);
+            var zoomh = (_substrateHeight / _stripsThicknees);
+            var zooms1 = _slots[0] / ((_slots[1] + _stripWidth) / 2);
+            var zooms2 = _slots[1] / ((_slots[0] + _stripWidth) / 2);
+            var zoomw = _stripWidth / ((_slots[0] + _slots[1]) / 2);
 
-            if (zoomh > 3)
-            {
-                zoomh = 3;
-            }
-            else if (zoomh < 0.2)
-            {
-                zoomh = 0.2;
-            }
-            if (zoomt > 3)
-            {
-                zoomt = 3;
-            }
-            else if (zoomt < 0.2)
-            {
-                zoomt = 0.2;
-            }
-
-            var S1 = 20 * zooms1+slot1/5;
-            var S2 = 20 * zooms2+slot2/ 5;
-            var W1 = 30 * zoomw+ stripWidth/ 5;
-            var t = 20 * zoomt+stripsThicknees/ 5;
-            var h = 20 * zoomh+substrateHeight/ 5;
-
+            var S1 = 20 * Zoom(zooms1) + _slots[0] / 5;
+            var S2 = 20 * Zoom(zooms2) + _slots[1] / 5;
+            var W1 = 30 * Zoom(zoomw) + _stripWidth / 5;
+            var t = 20 * Zoom(zoomt) + _stripsThicknees / 5;
+            var h = 20 * Zoom(zoomh) + _substrateHeight / 5;
 
             var wSolidBrush = new SolidColorBrush(Color.FromRgb(80, 80, 230));
             var hSolidBrsh = new SolidColorBrush(Color.FromRgb(140, 137, 126));
             var groundBrush = new SolidColorBrush(Colors.Black);
 
-            var textWidth = new FormattedText("W", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
-                new Typeface("verdana"), textSize, Brushes.Red);
-            var textS1 = new FormattedText("S1", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
-                new Typeface("verdana"), textSize, Brushes.Red);
-            var textS2 = new FormattedText("S2", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
-                new Typeface("verdana"), textSize, Brushes.Red);
-            var textSubstrateHeight = new FormattedText("h", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
-                new Typeface("verdana"), textSize, Brushes.Red);
-            var textThickness = new FormattedText("t", CultureInfo.GetCultureInfo("en-Us"), FlowDirection.LeftToRight,
-                new Typeface("verdana"), textSize, Brushes.Red);
 
+            var textWidth = Text("W", textSize);
+            var textS1 = Text("S1", textSize);
+            var textS2 = Text("S2", textSize);
+            var textSubstrateHeight = Text("h", textSize);
+            var textThickness = Text("t", textSize);
 
             var myPen = new Pen(Brushes.Black, 0.1);
             var penLine = new Pen(Brushes.Red, 0.5);
-            var substrateRect = new Rect(-(g + S1 + W1 / 2), 0, W1 + S1 + S2 + g * 2, h);
+            var substrateRect = new Rect(-(screen + S1 + W1 / 2), 0, W1 + S1 + S2 + screen * 2, h);
             var widthRect = new Rect(-W1 / 2, -t, W1, t);
-            var groundLeft = new Rect(-(g + S1 + W1 / 2), -t, g, t);
-            var groundRight = new Rect(W1 / 2 + S2, -t, g, t);
+            var groundLeft = new Rect(-(screen + S1 + W1 / 2), -t, screen, t);
+            var groundRight = new Rect(W1 / 2 + S2, -t, screen, t);
 
             dc.DrawRectangle(wSolidBrush, myPen, widthRect);
             dc.DrawRectangle(hSolidBrsh, myPen, substrateRect);
@@ -106,25 +95,25 @@ namespace GraphicModuleUI.ViewModels.Graphic
             dc.DrawRectangle(groundBrush, myPen, groundRight);
 
             //Линии разделения для слотов и ширины
-            dc.DrawLine(penLine, new Point(-(S1 + W1 / 2), -t), new Point(-(S1 + W1 / 2), -(t + 20)));
-            dc.DrawLine(penLine, new Point(-W1 / 2, -t), new Point(-W1 / 2, -(t + 20)));
-            dc.DrawLine(penLine, new Point(W1 / 2, -t), new Point(W1 / 2, -(t + 20)));
-            dc.DrawLine(penLine, new Point(W1 / 2 + S2, -t), new Point(W1 / 2 + S2, -(t + 20)));
-            dc.DrawLine(penLine, new Point(-(S1 + W1 / 2 + 5), -(t + 15)), new Point(W1 / 2 + S2 + 5, -(t + 15)));
+            dc.DrawLine(penLine, new Point(-(S1 + W1 / 2), -t), new Point(-(S1 + W1 / 2), -(t + 15)));
+            dc.DrawLine(penLine, new Point(-W1 / 2, -t), new Point(-W1 / 2, -(t + 15)));
+            dc.DrawLine(penLine, new Point(W1 / 2, -t), new Point(W1 / 2, -(t + 15)));
+            dc.DrawLine(penLine, new Point(W1 / 2 + S2, -t), new Point(W1 / 2 + S2, -(t + 15)));
+            dc.DrawLine(penLine, new Point(-(S1 + W1 / 2 + 5), -(t + 10)), new Point(W1 / 2 + S2 + 5, -(t + 10)));
 
-            dc.DrawText(textWidth, new Point(-2, -(t + 25)));
-            dc.DrawText(textS1, new Point(-(S1 / 2 + W1 / 2 + 4), -(t + 25)));
-            dc.DrawText(textS2, new Point(W1 / 2 + S2 / 2 - 4, -(t + 25)));
+            dc.DrawText(textWidth, new Point(-2, -(t + 20)));
+            dc.DrawText(textS1, new Point(-(S1 / 2 + W1 / 2 + 4), -(t + 20)));
+            dc.DrawText(textS2, new Point(W1 / 2 + S2 / 2 - 4, -(t + 20)));
 
             //Линии для разделения толщины подложки
-            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), 0), new Point(-(25 + g + S1 + W1 / 2), 0));
-            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), h), new Point(-(25 + g + S1 + W1 / 2), h));
-            dc.DrawText(textSubstrateHeight, new Point(-(30 + g + S1 + W1 / 2), h / 2 - 3));
+            dc.DrawLine(penLine, new Point(-(screen + S1 + W1 / 2), 0), new Point(-(10 + screen + S1 + W1 / 2), 0));
+            dc.DrawLine(penLine, new Point(-(screen + S1 + W1 / 2), h), new Point(-(10 + screen + S1 + W1 / 2), h));
+            dc.DrawText(textSubstrateHeight, new Point(-(15 + screen + S1 + W1 / 2), h / 2 - 3));
 
             //Линии для разделения толщины линии
-            dc.DrawLine(penLine, new Point(-(g + S1 + W1 / 2), -t), new Point(-(25 + g + S1 + W1 / 2), -t));
-            dc.DrawLine(penLine, new Point(-(20 + g + S1 + W1 / 2), -5 - t), new Point(-(20 + g + S1 + W1 / 2), h + 5));
-            dc.DrawText(textThickness, new Point(-(30 + g + S1 + W1 / 2), -t / 2 - 5));
+            dc.DrawLine(penLine, new Point(-(screen + S1 + W1 / 2), -t), new Point(-(10 + screen + S1 + W1 / 2), -t));
+            dc.DrawLine(penLine, new Point(-(5 + screen + S1 + W1 / 2), -5 - t), new Point(-(5 + screen + S1 + W1 / 2), h + 5));
+            dc.DrawText(textThickness, new Point(-(15 + screen + S1 + W1 / 2), -t / 2 - 5));
         }
     }
 }
